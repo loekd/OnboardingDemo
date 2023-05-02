e6e20367672e239f4de5f969308119bb387c6008
LoekD <loekd@hotmail.com>
2023-05-02 17:08:59 +0200

    fix(vnet): remove natgw

--- Infra/vnet.tf
+++ Infra/vnet.tf
@@ -31,51 +31,11 @@ resource "azurerm_subnet" "snet_pe_onboarding" {
     private_link_service_network_policies_enabled  = true
 }
 
-//subnet for outgoing traffic
-resource "azurerm_subnet" "snet_outbound_onboarding" {
-    name                 = "snet-outbound-onboarding"
-    address_prefixes     = [for x in local.address_prefix_onboarding : cidrsubnet(x, 5, 2)]
-    virtual_network_name = azurerm_virtual_network.vnet_onboarding.name
-    resource_group_name  = azurerm_resource_group.rg_onboarding.name
-}
-
-//nat gateway for outbound traffic
-resource "azurerm_nat_gateway" "nat_gateway" {
-  name                = "ng-onboarding"
-  location            = azurerm_resource_group.rg_onboarding.location
-  resource_group_name = azurerm_resource_group.rg_onboarding.name
-  idle_timeout_in_minutes = 4
-  sku_name = "Standard"  
-  
-}
-
-//public IP for nat gateway
-resource "azurerm_public_ip" "nat_gateway_pip" {
-  name                = "pip-nat-gateway"
-  location            = azurerm_resource_group.rg_onboarding.location
-  resource_group_name = azurerm_resource_group.rg_onboarding.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-//association between nat gateway and public IP
-resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_public_ip_association" {
-  nat_gateway_id       = azurerm_nat_gateway.nat_gateway.id
-  public_ip_address_id = azurerm_public_ip.nat_gateway_pip.id
-}
-
-//association between nat gateway and outbound subnet
-resource "azurerm_subnet_nat_gateway_association" "nat_gateway_snet_association" {
-  subnet_id      = azurerm_subnet.snet_outbound_onboarding.id
-  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
-}
-
 //private dns zones for ACR
 resource "azurerm_private_dns_zone" "private_dns_acr_onboarding" {
   name                = "privatelink.azurecr.io"
   resource_group_name = azurerm_resource_group.rg_onboarding.name
 }
-
 
 resource "azurerm_private_dns_zone_virtual_network_link" "private_link_acr_dns_onboarding" {
   name                  = "private_link_acr_dns_onboarding"
@@ -84,7 +44,6 @@ resource "azurerm_private_dns_zone_virtual_network_link" "private_link_acr_dns_o
   virtual_network_id    = azurerm_virtual_network.vnet_onboarding.id
   registration_enabled  = false
 }
-
 
 
 /////////////////////screening
@@ -122,13 +81,6 @@ resource "azurerm_subnet" "snet_pe_screening" {
     private_link_service_network_policies_enabled  = true
 }
 
-//subnet for outgoing traffic
-resource "azurerm_subnet" "snet_outbound_screening" {
-    name                 = "snet-outbound-screening"
-    address_prefixes     = [for x in local.address_prefix_screening : cidrsubnet(x, 5, 2)]
-    virtual_network_name = azurerm_virtual_network.vnet_screening.name
-    resource_group_name  = azurerm_resource_group.rg_screening.name
-}
 
 resource "azurerm_private_dns_zone_virtual_network_link" "private_link_acr_dns_screening" {
   name                  = "private_link_acr_dns_screening"
